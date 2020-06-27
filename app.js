134c69637ce5b8e8d34ed5a352aff66210895207
Alson Shareef <alson.shareef@gmail.com>
2020-06-27 19:16:25 +0800

    Error 404 route

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+// Error 404
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('<h1>Page does not exist.</h1>');
+});
+
 // PORT
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

e817dbb347a74a01a6af8e54f60efe1bd5c28d30
Alson Shareef <alson.shareef@gmail.com>
2020-06-27 20:06:14 +0800

    used node path module to send add-product HTML page.

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // Add product route
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="product" /><button type="submit">Add product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // Post route for product at same path.

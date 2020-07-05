def8d3b493ff46a50430fdb1738b38cf66d9e07a
Alson Shareef <alson.shareef@gmail.com>
2020-07-05 21:36:54 +0800

    Shop routes now access middleware callbacks from shop controller file. Also added Index, Products, Cart and Checkout routes.

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const productController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productController.getProducts);
+// GET index Page.
+router.get('/', shopController.getIndex);
+
+// GET products page.
+router.get('/products', shopController.getProducts);
+
+// GET items currently added to shopping cart.
+router.get('/cart', shopController.getCart);
+
+// GET checkout page for purchasing items in shopping cart.
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;

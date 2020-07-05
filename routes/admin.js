c024b2294ee2cf50a5f53798ce004f8bdd4104cc
Alson Shareef <alson.shareef@gmail.com>
2020-07-05 21:33:34 +0800

    Admin routes now access controller methods from an admin controller file and added get route for admin product list.

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // GET add product page.
-router.get('/add-product', productController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// GET admin version of products page.
+router.get('/admin-product-list', adminController.getProducts);
 
 // POST new product.
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;

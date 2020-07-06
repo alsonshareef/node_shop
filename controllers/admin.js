76a88d25475c12b971476d104b4c8761ceb50fcb
Alson Shareef <alson.shareef@gmail.com>
2020-07-06 17:59:11 +0800

    Admin controller for getting the edit-product page, and product modal now stores imageURL, description and price.

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,14 +18,27 @@ exports.getProducts = (req, res, next) => {
     res.render('admin/admin-product-list', {
       prods: products,
       pageTitle: 'Admin Products',
-      path: '/admin-product-list',
+      path: '/admin/admin-product-list',
     });
+  });
+};
+
+// GET edit page for products that only admin can access.
+exports.getEditProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: 'admin/edit-product',
   });
 };
 
 // POST new product to store.
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const product = new Product(
+    req.body.title,
+    req.body.imageURL,
+    req.body.description,
+    req.body.price
+  );
   product.save();
   res.redirect('/products');
 };

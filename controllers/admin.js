cf44f7806e08bc3e0e45aec3874c53d21dd2545a
Alson Shareef <alson.shareef@gmail.com>
2020-07-09 18:57:47 +0800

    Created getEditProduct and postEditProduct controller methods.

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,10 +6,23 @@ const Product = require('../models/product');
 
 // GET page to add a new product to store.
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
+};
+
+// POST new product to store.
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(
+    req.body.title,
+    req.body.imageURL,
+    req.body.description,
+    req.body.price
+  );
+  product.save();
+  res.redirect('/products');
 };
 
 // GET admin version of page displaying current products in store.
@@ -25,20 +38,24 @@ exports.getProducts = (req, res, next) => {
 
 // GET edit page for products that only admin can access.
 exports.getEditProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: 'admin/edit-product',
+  const editMode = req.query.edit;
+  const prodId = req.params.productId;
+  Product.findProductbyID(prodId, (product) => {
+    if (!product) {
+      res.redirect('/');
+      alert('Product you are trying to edit does not exist.');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: 'admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
 // POST new product to store.
-exports.postAddProduct = (req, res, next) => {
-  const product = new Product(
-    req.body.title,
-    req.body.imageURL,
-    req.body.description,
-    req.body.price
-  );
-  product.save();
+exports.postEditProduct = (req, res, next) => {
   res.redirect('/products');
+  console.log('edited product.');
 };

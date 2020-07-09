a728492e90171204dd7ee718b05c6356e71d385b
Alson Shareef <alson.shareef@gmail.com>
2020-07-09 19:01:26 +0800

    GET edit-product page route and POST edit-product route for admin users.

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,16 @@ const router = express.Router();
 // GET add product page.
 router.get('/add-product', adminController.getAddProduct);
 
+// POST new product.
+router.post('/add-product', adminController.postAddProduct);
+
 // GET admin version of products page.
 router.get('/admin-product-list', adminController.getProducts);
 
 // GET edit product page for admins.
-router.get('/edit-product', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// POST new product.
-router.post('/add-product', adminController.postAddProduct);
+// POST edits made for a product.
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;

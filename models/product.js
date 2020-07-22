57242d0fb39c021e9ce3a47f7737bb0e17c349eb
Alson Shareef <alson.shareef@gmail.com>
2020-07-22 22:04:44 +0800

    Updated product modal methods to write SQL queries to MYSQL database instead of writing to data file.

--- models/product.js
+++ models/product.js
@@ -2,24 +2,9 @@
  * PRODUCT MODAL
  */
 
-const fs = require('fs');
-const path = require('path');
-const rootDir = require('../helpers/root_dir');
-
-const productDataPath = path.join(rootDir, 'data', 'products.json');
+const db = require('../helpers/database');
 
 const Cart = require('./cart');
-
-// Retrieves JSON data of products and converts to object.
-const getProductsFromFile = (callback) => {
-  fs.readFile(productDataPath, (err, fileContent) => {
-    if (err) {
-      callback([]);
-    } else {
-      callback(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageURL, description, price) {
@@ -31,56 +16,19 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile((products) => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(
-          (product) => product.id === this.id
-        );
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(
-          productDataPath,
-          JSON.stringify(updatedProducts),
-          (err) => {
-            console.log(`Error: ${err}`);
-          }
-        );
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(productDataPath, JSON.stringify(products), (err) => {
-          console.log(`Error: ${err}`);
-        });
-      }
-    });
+    return db.execute(
+      'INSERT INTO products (title, price, imageURL, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageURL, this.description]
+    );
   }
 
-  static fetchAll(callback) {
-    getProductsFromFile(callback);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
   }
 
-  static findProductById(id, callback) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id);
-      callback(product);
-    });
+  static findProductById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 
-  static deleteProductById(id) {
-    getProductsFromFile((products) => {
-      // Product to be deleted
-      const product = products.find((product) => product.id === id);
-
-      // New set of products after product was deleted
-      const updatedProducts = products.filter((product) => product.id !== id);
-
-      // Update products json data with new updatedProducts and delete products from cart.
-      fs.writeFile(productDataPath, JSON.stringify(updatedProducts), (err) => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price);
-        }
-        console.log(`Error: ${err}`);
-      });
-    });
-  }
+  static deleteProductById(id) {}
 };

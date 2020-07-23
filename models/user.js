7b6266a9a2540fe134e103bf930f9c2d087844b8
Alson Shareef <alson.shareef@gmail.com>
2020-07-23 17:51:58 +0800

    Created User modal with Sequelize.

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../helpers/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = User;

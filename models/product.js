be1428c2ad88d7dbbf9712c04e1a65c749852d74
Alson Shareef <alson.shareef@gmail.com>
2020-07-23 15:14:01 +0800

    Defined a Product modal with Sequelize

--- models/product.js
+++ models/product.js
@@ -1,34 +1,28 @@
-/**
- * PRODUCT MODAL
- */
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../helpers/database');
 
-const db = require('../helpers/database');
+const Product = sequelize.define('product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+  },
+  price: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  imageURL: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-const Cart = require('./cart');
-
-module.exports = class Product {
-  constructor(id, title, imageURL, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageURL = imageURL;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageURL, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageURL, this.description]
-    );
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findProductById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-
-  static deleteProductById(id) {}
-};
+module.exports = Product;

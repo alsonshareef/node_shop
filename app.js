9641d4d969c325a7f4bdd8320f49ef80148dbd0e
Alson Shareef <alson.shareef@gmail.com>
2020-07-23 15:11:54 +0800

    Server now runs after sequelize has synced the models to the db.

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./helpers/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+const PORT = process.env.PORT || 5000;
 
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,8 +26,13 @@ app.use(shopRoutes);
 // Error 404
 app.use(errorController.get404);
 
-// PORT
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+// Run server after Sequelize models have synced to the db and created their respective tables.
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));

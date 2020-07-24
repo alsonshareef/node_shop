5ea9fb3b927c38146984d7de7ba628176b9a9912
Alson Shareef <alson.shareef@gmail.com>
2020-07-24 18:31:51 +0800

    Completed all Sequelize Modal Associations

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./helpers/database');
+
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -43,12 +46,18 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 /**
- * APP INIT
+ * MODAL ASSOCIATIONS
  */
-// Establish entity relationships
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
+/**
+ * APP INIT
+ */
 // Run server after Sequelize models have synced to the db and created their respective tables.
 sequelize
   // .sync({ force: true }) // -- Run this if tables need to be reset.
@@ -69,7 +78,9 @@ sequelize
     return user;
   })
   .then((user) => {
-    console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });

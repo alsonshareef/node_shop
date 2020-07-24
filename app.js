86c0cba70a128f012528e7a82c1520d6037e669b
Alson Shareef <alson.shareef@gmail.com>
2020-07-24 16:59:34 +0800

    A new User is now created if it doesn't already exist on app init, and there is a middleware which attaches the user to subsequent incoming requests.

--- app.js
+++ app.js
@@ -10,27 +10,66 @@ const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./helpers/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const PORT = process.env.PORT || 5000;
 
-// MIDDLEWARE
+/**
+ * MIDDLEWARE
+ */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ROUTES
+// Stores the dummy user created or already existing from app init into the incoming request.
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+/**
+ * ROUTES
+ */
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // Error 404
 app.use(errorController.get404);
 
+/**
+ * APP INIT
+ */
+// Establish entity relationships
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 // Run server after Sequelize models have synced to the db and created their respective tables.
 sequelize
+  // .sync({ force: true }) // -- Run this if tables need to be reset.
   .sync()
   .then((result) => {
-    // console.log(result);
+    // Find a user.
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    // Create a new user if user doesn't already exist and return new user || return existing user.
+    if (!user) {
+      return User.create({
+        firstName: 'Alson',
+        lastName: 'Shareef',
+        email: 'test@email.com',
+      });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
